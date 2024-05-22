89875479641ff07495b4dcb7c0339f260b86a257
hungyu01 <linhungyu0328@gmail.com>
2024-05-22 23:41:02 +0800

    登入登出功能，製作session中

--- accounts/app.js
+++ accounts/app.js
@@ -3,13 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/web/index');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const {DBHOST, DBPORT, DBNAME} = require('./config/config');
+const indexRouter = require('./routes/web/index');
 const authRouter = require('./routes/web/auth');
 //導入 account api 路由文件
 const accountRouter = require('./routes/api/account');
 
 var app = express();
+
+//設定 session 的中間件
+app.use(session({
+  name: 'sid', //設定 cookie 的 name，預設是: connect.sid
+  secret: 'hung-yu', //參與加密的字符串(又稱為簽名)
+  saveUninitialized: false, //是否在每次請求時都設定一個 cookie 儲存 session id
+  resave: true, //在每次請求後重新保存 session
+  store: MongoStore.create({
+      mongoUrl: `mongodb://${DBHOST}:${DBPORT}/${DBNAME}` //資料庫的連接
+  }),
+  cookie:{
+      httpOnly:true, //開啟後前端無法透過 JS 操作
+      maxAge: 1000*60*60*24*7 //控制 session 過期的時間
+  },
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- accounts/routes/web/auth.js
+++ accounts/routes/web/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 //導入 UserModel 模型
 const UserModel = require('../../models/UserModel')
 //導入 md5 加密密碼
@@ -30,6 +31,28 @@ router.post('/reg', async(req, res)=>{
 router.get('/login', (req, res)=>{
     //回應html內容
     res.render('auth/login');
+});
+
+// 登入的操作
+router.post('/login', async (req, res) => {
+    // 取得用戶名跟密碼
+    let { username, password } = req.body;
+
+    try {
+        // 查詢資料庫密碼
+        const user = await UserModel.findOne({ username: username, password: md5(password) });
+
+        if (user) {
+            req.session.username = user.username;
+            req.session._id = user._id;
+            res.render('success', { msg: '登入成功', url: '/login' });
+        } else {
+            res.status(500).send('帳號或密碼錯誤，請稍後再試');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('帳號或密碼錯誤，請稍後再試');
+    }
 });
 
 module.exports = router;
